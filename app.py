feb0ac49cc63c408f48f4cf3579222648d83a08e
Kazekage19 <akshat.srivastava.ug20@nsut.ac.in>
2023-02-12 15:47:41 +0530

    Just not deployed

--- app.py
+++ app.py
@@ -14,25 +14,32 @@ model=objects[0]
 scalar=objects[1]
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())))
-    output=model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+# @app.route('/predict_api',methods=['POST','GET'])
+# def predict_api():
+#     data=request.json['data']
+#     print(data)
+#     print(np.array(list(data.values())).reshape(1,7,1))
+#     new_data=scalar.transform(np.array(list(data.values())).reshape(1,7,1))
+#     output=model.predict(new_data)
+#     print(output[0])
+#     return jsonify(output)
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    # data=[float(x) for x in request.form.values()]
+    # final_input=scalar.transform(np.array(data).reshape(1,7,1))
+    # print(final_input)
+    # output=model.predict(final_input)[0]
+    # return render_template("index.html",prediction_text="Outputs {}".format(output))
+    form_data=request.form 
+    json_data=json.dumps(form_data)
+    data=json.loads(json_data)
+    inp=np.array(data.values())
+    out=model.predict(inp)
+    return out
 
 
 
